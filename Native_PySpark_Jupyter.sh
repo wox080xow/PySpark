fa0c785dc82f25de64c519f64796effb7bd12e06
wox080xow <b04302127@g.ntu.edu.tw>
2021-01-18 21:07:50 +0800

    update output

--- Native_PySpark_Jupyter.sh
+++ Native_PySpark_Jupyter.sh
@@ -5,7 +5,7 @@ export PYSPARK_DRIVER_PYTHON="jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
 
 # Start PySpark local mode
-#pyspark
+pyspark
 
 # Start PySpark YARN mode
 # pyspark --master yarn --deploy-mode cluster \ # Error
@@ -16,8 +16,8 @@ export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
 #--executor-memory 2G
 
 # Start PySpark Standalone mode
-pyspark --master yarn --deploy-mode client \
---driver-memory 1G \
---num-executors 1 \
---executor-cores 1 \
---executor-memory 2G
+#pyspark --master "spark://bdse125.example.org:7077" \
+#--driver-memory 1G \
+#--num-executors  1\
+#--executor-cores 1 \
+#--executor-memory 2G
